1d89c05fad052d897c12872963b10c54c8e59458
Yann Klein <yann.klein@me.com>
2023-05-20 18:09:49 +0900

    livecode done

--- task.rb
+++ task.rb
@@ -1,47 +1,57 @@
 class Task
   attr_reader :id
-  attr_accessor :description, :title, :done
+  attr_accessor :done, :title, :description
+
   def initialize(attributes = {})
-    @id = attributes[:id] || attributes['id']
-    @title = attributes[:title] || attributes['title']
-    @description = attributes[:description] || attributes['description']
-    # done is a boolean in the task instance
-    # done is an integer in the DB!😈
-    @done = attributes[:done] || @done = attributes['done'] || false
+    @id = attributes[:id] || attributes["id"]
+    @title = attributes[:title] || attributes["title"]
+    @description = attributes[:description] || attributes["description"]
+    @done = attributes[:done] || attributes["done"] || false
   end
 
   def self.find(id)
-    result = DB.execute("SELECT * FROM tasks WHERE id = ?", id).first
-    # result is a hash!
-    # REFACTOR!! 👇
-    result['done'] = result['done'] == 1
-    Task.new(result)
+    # create a SQL query
+    query = "SELECT * FROM tasks WHERE id = ?"
+    # execute it -> array of one hash with some task info
+    DB.results_as_hash = true
+    task_info = DB.execute(query, id).first
+    # {
+    #   "id" => 4, 
+    #   "title" => "Coding", 
+    #   "description" => "google your problem", 
+    #   "done" => 0
+    # }
+
+    # if task_info["done"] is 1 
+    # task_info["done"] == 1 ===> true
+    # if task_info["done"] is 0 
+    # task_info["done"] == 1 ===> false
+
+    task_info["done"] = task_info["done"] == 1
+    # create an instance of task with the task info
+    # return an instance of task
+    Task.new(task_info)
   end
 
   def save
-    mark_done = @done ? 1 : 0
     if @id.nil?
-      DB.execute("INSERT INTO tasks (title, description, done) VALUES (?, ?, ?)", @title, @description, mark_done)
+      # create the code
+      DB.execute("INSERT INTO tasks(title, description, done) VALUES(?,?,?)",@title, @description, @done ? 1 : 0)
       @id = DB.last_insert_row_id
     else
-      DB.execute("UPDATE tasks SET title = ?, description = ?, done = ? WHERE id = ?", @title, @description, mark_done, @id)
+      # update the code
+      query = <<~SQL
+        UPDATE tasks
+        SET title = ?, description = ?, done = ?
+        WHERE id = ?
+      SQL
+      DB.execute(query, @title, @description, @done ? 1 : 0, @id)
     end
   end
 
-  def self.all
-    query = <<~SQL
-      SELECT * FROM tasks
-    SQL
-    rows = DB.execute(query)
-    rows.map { |row| Task.new(row) }
-  end
 
   def destroy
-    query = <<~SQL
-      DELETE FROM tasks
-      WHERE id = ?
-    SQL
+    query = "DELETE FROM tasks WHERE id = ?"
     DB.execute(query, @id)
   end
 end
-# where do we turn the 0 into false, before creating our task?
--- test.rb
+++ test.rb
@@ -1,24 +1,33 @@
 require "sqlite3"
 require "pry-byebug"
 DB = SQLite3::Database.new("tasks.db")
-DB.results_as_hash = true
+
 require_relative "task"
 
+# create a model of Task with title (String), description (String), done (boolean)
+# p Task.new(title: 'Livecode', description: 'code live')
+
 # Implement the READ logic to find a given task (by its id)
-task = Task.find(3)
-puts "#{task.title} [#{task.done ? "X" : " "}] - #{task.description}"
+# task = Task.find(4)
+# p task
+# puts "[#{task.done ? "X" : " "}] #{task.title}: #{task.description}"
 
 # Implement the CREATE logic in a save instance method
-# task = Task.new(title: "Coding", description: "google your problem")
-# task.save
+# new_task = Task.new(title: 'Livecode', description: 'code live')
+# new_task.save
 
 # Implement the UPDATE logic in the same method
+# task = Task.find(4)
+# task.description = "ask your problem to ChatGPT"
 # task.done = true
 # task.save
 
 # Implement the READ logic to retrieve all tasks (what type of method is it?)
-p Task.all
+tasks = Task.all
+tasks.each { |task| puts "[#{task.done ? "X" : " "}] #{task.title}: #{task.description}"}
 
 # Implement the DESTROY logic on a task
-task_to_destroy = Task.find(3)
-task_to_destroy.destroy
+task = Task.find(4)
+task.destroy
+tasks = Task.all
+tasks.each { |task| puts "[#{task.done ? "X" : " "}] #{task.title}: #{task.description}"}
